cf1a207b5b93838180bc4d6fd5bcad8dc270ba6d
Fabian Cabau <fabiancabau@hotmail.com>
2015-06-26 18:26:45 -0300

    next step: auth

--- app.js
+++ app.js
@@ -17,114 +17,27 @@ app.use(express.static(path.join(__dirname, '/')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/index.html');
-// });
-
-// error handler
-// app.use(function (err, req, res, next) {
-//   console.error(err.stack);
-//   res.status(400).send(err.message);
-// });
-
-
-var Bear     = require('./models/bear');
-
-
 // ===============================================================
 
 var mongoose   = require('mongoose');
 mongoose.connect('mongodb://localhost'); // connect to our database
 
 
-// ROUTES FOR THE API
-// =============================================================================
-var router = express.Router(); 
-
-// middleware to use for all requests
-router.use(function(req, res, next) {
-    // do logging
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
-});
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
-
-
-// more routes for our API will happen here
-
-
-// /bears
-router.route('/bears')
-
-	//create a bear
-	.post(function(req, res) {
-
-		var bear = new Bear();
-		bear.name = req.body.name;
-
-		bear.save(function(err) {
-			if (err)
-				res.send(err);
-
-			res.json({ message: 'Bear created!' });
-		});
-
-	})
-
-	.get(function(req, res) {
-		Bear.find(function(err, bears){
-			if (err)
-				res.send(err);
-
-			res.json(bears);
-		});
-	});
-
-
-// /bears/:bear_id
-router.route('/bears/:bear_id')
-
-    // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
-    .get(function(req, res) {
-        Bear.findById(req.params.bear_id, function(err, bear) {
-            if (err)
-                res.send(err);
-            res.json(bear);
-        });
-    })
-
-    // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
-    .put(function(req, res) {
-
-        // use our bear model to find the bear we want
-        Bear.findById(req.params.bear_id, function(err, bear) {
-
-            if (err)
-                res.send(err);
-
-            bear.name = req.body.name;  // update the bears info
-
-            // save the bear
-            bear.save(function(err) {
-                if (err)
-                    res.send(err);
-
-                res.json({ message: 'Bear updated!' });
-            });
-
-        });
-    });
+// GET OUR ROUTERS ----------------------------------
+var bear_router 	= require('./routes/bear_routes'),
+	client_router 	= require('./routes/client_routes'),
+	project_router	= require('./router/project_routes'); 
 
 
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api', router);
+app.use('/api', bear_router);
+app.use('/api', client_router);
+app.use('/api', project_router);
 
 
-app.listen(port);
-console.log('Server listening on port ' + port);
+app.listen(port, function(){
+	console.log('Server listening on port ' + port);
+});
+
